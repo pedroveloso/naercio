b656b9328345e84c062388f555d8f9f068694b97
pedroveloso <pedrohenriqueveloso@hotmail.com>
2016-11-18 20:06:58 -0200

    Select inicial, corrigir para incluir vars de pareamento

--- PSM_Inaf.R
+++ PSM_Inaf.R
@@ -1,2 +1,15 @@
 library(dplyr)
 library(MatchIt)
+library(ggplot2)
+
+
+dados <- read.csv(file.choose(),sep=";",na.strings = ".")
+
+# Ajustar esse selelct para conter as variaveis de pareamento
+dados <- dados %>% select(.,c(quest,c(ProfComb:se4_n)))
+
+dadosT <- dados %>% mutate(Alfab = ifelse(ProfComb <= 95,0,1)) %>% 
+  mutate(Profic = ifelse(ProfComb > 137,1,0)) %>% 
+  mutate(NCS1 = ifelse(c_thet > median(c_thet),1,0)) %>%
+  mutate(NCS2 = ifelse(o_thet > median(o_thet),1,0)) %>% 
+  mutate(NCS3 = ifelse(se_thet > median(se_thet),1,0))
