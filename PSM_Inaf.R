b55443b63b0a23050694b1a13126de50ff61fa08
pedroveloso <pedrohenriqueveloso@hotmail.com>
2016-11-22 02:58:50 -0200

    Adicionando Vars. Pareamento

--- PSM_Inaf.R
+++ PSM_Inaf.R
@@ -1,15 +1,18 @@
 library(dplyr)
-library(MatchIt)
 library(ggplot2)
 
 
 dados <- read.csv(file.choose(),sep=";",na.strings = ".")
 
 # Ajustar esse selelct para conter as variaveis de pareamento
-dados <- dados %>% select(.,c(quest,c(ProfComb:se4_n)))
+dados <- dados %>% select(.,c(quest,sexo,raca,id_real,c(p12:p13),c(ProfComb:se4_n)))
 
 dadosT <- dados %>% mutate(Alfab = ifelse(ProfComb <= 95,0,1)) %>% 
   mutate(Profic = ifelse(ProfComb > 137,1,0)) %>% 
   mutate(NCS1 = ifelse(c_thet > median(c_thet),1,0)) %>%
   mutate(NCS2 = ifelse(o_thet > median(o_thet),1,0)) %>% 
-  mutate(NCS3 = ifelse(se_thet > median(se_thet),1,0))
+  mutate(NCS3 = ifelse(se_thet > median(se_thet),1,0)) %>% 
+  mutate(sexoT = ifelse(sexo == 1, 1, 0)) %>% 
+  mutate(racaT = ifelse(raca == 1, 1, 0)) %>% 
+  mutate(EscolPai = ifelse(p12 == 99, 0, p12)) %>% 
+  mutate(EscolMae = ifelse(p13 == 99, 0, p13))
