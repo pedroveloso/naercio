977a8814259bdd328f049a436693a205b4074d17
Pedro Veloso <pedrohenriqueveloso@hotmail.com>
2016-11-27 11:46:20 -0200

    minor change in varnames

--- PSM_Inaf.R
+++ PSM_Inaf.R
@@ -10,77 +10,77 @@ dados <- read.csv(file.choose(),sep=";",na.strings = ".")
 # Ajustar esse selelct para conter as variaveis de pareamento
 dados <- dados %>% select(.,c(quest,sexo,raca,id_real,c(p12:p13),c(ProfComb:se4_n)))
 
-dadosT <- dados %>% mutate(Alfab = ifelse(ProfComb <= 95,0,1)) %>% 
-  mutate(Profic = ifelse(ProfComb > 137,1,0)) %>% 
+dadosT <- dados %>% mutate(alfab = ifelse(ProfComb <= 95,0,1)) %>% 
+  mutate(profic = ifelse(ProfComb > 137,1,0)) %>% 
   mutate(NCS1 = ifelse(c_thet > median(c_thet),1,0)) %>%
   mutate(NCS2 = ifelse(o_thet > median(o_thet),1,0)) %>% 
   mutate(NCS3 = ifelse(se_thet > median(se_thet),1,0)) %>% 
   mutate(sexoT = ifelse(sexo == 1, 1, 0)) %>% 
   mutate(racaT = ifelse(raca == 1, 1, 0)) %>% 
-  mutate(EscolPai = ifelse(p12 == 99, 0, p12)) %>% 
-  mutate(EscolMae = ifelse(p13 == 99, 0, p13))
+  mutate(escolPai = ifelse(p12 == 99, 0, p12)) %>% 
+  mutate(escolMae = ifelse(p13 == 99, 0, p13))
 
 
 #Comparacao sem pareamento para alfabetizados
-vars_paream <- c('sexo','raca','id_real','EscolPai','EscolMae')
+vars_paream <- c('sexo','raca','id_real','escolPai','escolMae')
 
-mediasSemParAlfab <- dadosT %>% group_by(Alfab) %>% summarise(n_particp = n(), 
+mediasSemParalfab <- dadosT %>% group_by(alfab) %>% summarise(n_particp = n(), 
                                                          mean_c = mean(c_thet), 
                                                          mean_o = mean(o_thet), 
                                                          mean_se = mean(se_thet))
 
-tabelaVarsPareamAlfab <- dadosT %>% group_by(Alfab) %>%
+tabelaVarsPareamalfab <- dadosT %>% group_by(alfab) %>%
   select(one_of(vars_paream)) %>% 
   summarise_all(funs(mean(.,na.rm=T)))
 
-listaTestsAlfab <- lapply(vars_paream, function(v){
-  t.test(dadosT[,v] ~ dadosT[,'Alfab'])
+listaTestsalfab <- lapply(vars_paream, function(v){
+  t.test(dadosT[,v] ~ dadosT[,'alfab'])
 })
 
 #Comparacao sem pareamento para proficientes
-mediasSemParProfic <- dadosT %>% group_by(Profic) %>% summarise(n_particp = n(), 
+mediasSemParprofic <- dadosT %>% group_by(profic) %>% summarise(n_particp = n(), 
                                                                 mean_c = mean(c_thet), 
                                                                 mean_o = mean(o_thet), 
                                                                 mean_se = mean(se_thet))
 
-tabelaVarsPareamProfic <- dadosT %>% group_by(Profic) %>%
+tabelaVarsPareamprofic <- dadosT %>% group_by(profic) %>%
   select(one_of(vars_paream)) %>% 
   summarise_all(funs(mean(.,na.rm=T)))
 
-listaTestsProfic <- lapply(vars_paream, function(v){
-  t.test(dadosT[,v] ~ dadosT[,'Profic'])
+listaTestsprofic <- lapply(vars_paream, function(v){
+  t.test(dadosT[,v] ~ dadosT[,'profic'])
 })
 
 # Propensity Score para alfabetizados
-alfabPSModel <- glm(Alfab ~ sexoT + racaT + EscolPai + EscolMae, family = binomial(), data = dadosT)
+alfabPSModel <- glm(alfab ~ sexoT + racaT + escolPai + escolMae + id_real, family = binomial(), data = dadosT)
 summary(alfabPSModel)
 
 alfabPredicted <- data.frame(alfabPScore = predict(alfabPSModel, type = "response"),
-                             Alfab = alfabPSModel$model$Alfab)
+                             alfab = alfabPSModel$model$alfab)
 
 # Propensity Score para proficientes
-proficPSModel <- glm(Profic ~ sexoT + racaT + EscolPai + EscolMae, family = binomial(), data = dadosT)
+proficPSModel <- glm(profic ~ sexoT + racaT + escolPai + escolMae + id_real , family = binomial(), data = dadosT)
 summary(proficPSModel)
 
 proficPredicted <- data.frame(proficPScore = predict(proficPSModel, type = "response"),
-                             Profic = proficPSModel$model$Profic)
+                             profic = proficPSModel$model$profic)
 
 # Avaliacao da regiao de suporte comum para alfabetizados
 rotulos <- paste("Categoria de alfatebitação: ", c("Alfabetizados","Analfabetos funcionais"))
-alfabPredicted %>% mutate(Alfab = ifelse(Alfab == 1, rotulos[1],rotulos[2])) %>%
+alfabPredicted %>% mutate(alfab = ifelse(alfab == 1, rotulos[1],rotulos[2])) %>%
   ggplot(aes(x = alfabPScore)) +
   geom_histogram(color = "white") +
-  facet_wrap(~Alfab) +
+  facet_wrap(~alfab) +
   xlab("Probabilidade de ser alfabetizado") +
   theme_bw()
 
 # Avaliacao da regiao de suporte comum para proficientes
-rotulos <- paste("Categoria de alfatebitação: ", c("Proficientes","Não proficientes"))
-proficPredicted %>% mutate(Profic = ifelse(Profic == 1, rotulos[1],rotulos[2])) %>%
+rotulos <- paste("Categoria de alfatebitação: ", c("Proficientes","Não Proficientes"))
+proficPredicted %>% mutate(profic = ifelse(profic == 1, rotulos[1],rotulos[2])) %>%
   ggplot(aes(x = proficPScore)) +
   geom_histogram(color = "white") +
-  facet_wrap(~Profic) +
-  xlab("Probabilidade de ser alfabetizado") +
+  facet_wrap(~profic) +
+  xlab("Probabilidade de ser proficiente") +
   theme_bw()
 
 
