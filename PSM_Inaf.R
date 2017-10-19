956125bbdd7c9744e3a9a7013f2b62684440c7d5
pedroveloso <pedrohenriqueveloso@hotmail.com>
2017-10-19 00:53:51 -0200

    Separanting only students who get into high school

--- PSM_Inaf.R
+++ PSM_Inaf.R
@@ -13,7 +13,7 @@ dadosT <- dados %>% select(.,c(quest,sexo,raca,id_real,EST,p1:p3, p502,c(p12:p13
 dadosT <- dados %>% mutate(alfab = ifelse(ProfComb <= 95, 0, 1)) %>% 
   mutate(profic = ifelse(ProfComb > 137, 1, 0)) %>% 
   mutate(Autogestao = ifelse(c_thet > median(c_thet), 1, 0)) %>%
-  mutate(Openness = ifelse(o_thet > median(o_thet), 1, 0)) %>% 
+  mutate(AberturaNovo = ifelse(o_thet > median(o_thet), 1, 0)) %>% 
   mutate(Autoconceito = ifelse(se_thet > median(se_thet), 1, 0)) %>% 
   mutate(sexoT = ifelse(sexo == 1, 1, 0)) %>% 
   mutate(racaT = ifelse(raca == 1, 1, 0)) %>%
@@ -27,10 +27,10 @@ dadosT <- dados %>% mutate(alfab = ifelse(ProfComb <= 95, 0, 1)) %>%
   mutate(ensinoFundCompleto = ifelse(p1 >= 1 & p1 <= 8, 0, 1)) %>% 
   mutate(ensinoMedioCompleto = ifelse(p1 >= 1 & p1 <= 11, 0, 1 )) %>% 
   mutate(aindaEstuda = ifelse(p3 == 1, 1, 0)) %>% 
-  filter(p1 <= 12) %>% 
+  filter(p1 <= 12 & p1 >= 9) %>% 
   filter(p502 != 2) %>% 
   mutate(idade1serie = ifelse(p1 == 1, 0, p2)) %>% 
-  filter(id_real > 25)
+  filter(id_real > 18)
 
 # Pensar sobre a questão da idade de ingresos na escola...
 
@@ -40,7 +40,7 @@ dadosT <- dadosT %>% mutate(c4_n = as.character(c4_n)) %>%
   mutate(se4_n = as.character(se4_n)) %>% 
   filter(c4_n == "limited" | c4_n == "highly" | o4_n == "limited" | o4_n == "highly") %>% 
   mutate(Autogestao = ifelse(c4_n == 'highly', 1, 0)) %>% 
-  mutate(Openness = ifelse(o4_n == 'highly', 1, 0))
+  mutate(AberturaNovo = ifelse(o4_n == 'highly', 1, 0))
 
 #Comparacao sem pareamento para HSEs
 
@@ -51,7 +51,7 @@ tabelaVarsPareamAutogestao <- dadosT %>% group_by(Autogestao) %>%
   select(one_of(vars_paream)) %>% 
   summarise_all(funs(mean(.,na.rm=T)))
 
-tabelaVarsPareamOpenness <- dadosT %>% group_by(Openness) %>%
+tabelaVarsPareamAberturaNovo <- dadosT %>% group_by(AberturaNovo) %>%
   select(one_of(vars_paream)) %>% 
   summarise_all(funs(mean(.,na.rm=T)))
 
@@ -62,8 +62,8 @@ listaTestsAutogestao <- lapply(vars_paream, function(v){
   t.test(dadosT[,v] ~ dadosT[,'Autogestao'])
 })
 
-listaTestsOpenness <- lapply(vars_paream, function(v){
-  t.test(dadosT[,v] ~ dadosT[,'Openness'])
+listaTestsAberturaNovo <- lapply(vars_paream, function(v){
+  t.test(dadosT[,v] ~ dadosT[,'AberturaNovo'])
 })
 
 # Propensity Score para Autogestao
@@ -76,17 +76,17 @@ summary(AutogestaoPSModel)
 AutogestaoPredicted <- data.frame(AutogestaoPScore = predict(AutogestaoPSModel, type = "response"), 
                                   Autogestao = AutogestaoPSModel$model$Autogestao)
 
-# Propensity Score para Openness
-OpennessPSModel <- glm(Openness ~ id_real + idade1serie + sexoT + racaT +
+# Propensity Score para AberturaNovo
+AberturaNovoPSModel <- glm(AberturaNovo ~ id_real + idade1serie + sexoT + racaT +
                          maeMedioCompleto,
                        family = binomial(), data = dadosT)
-summary(OpennessPSModel)
+summary(AberturaNovoPSModel)
 
-OpennessPredicted <- data.frame(OpennessPScore = predict(OpennessPSModel, type = "response"), 
-                                Openness = OpennessPSModel$model$Openness)
+AberturaNovoPredicted <- data.frame(AberturaNovoPScore = predict(AberturaNovoPSModel, type = "response"), 
+                                AberturaNovo = AberturaNovoPSModel$model$AberturaNovo)
 
 # Avaliacao da regiao de suporte comum para Autogestao
-rotulos <- paste("HSE - Autogestao: ", c("Acima da mediana","Abaixo da mediana"))
+rotulos <- paste("HSE - Autogestao: ", c("Muito Desenvolvido","A desenvolver"))
 AutogestaoPredicted %>% mutate(Autogestao = ifelse(Autogestao == 1, rotulos[1],rotulos[2])) %>%
   ggplot(aes(x = AutogestaoPScore)) +
   geom_histogram(color = "white") +
@@ -94,18 +94,18 @@ AutogestaoPredicted %>% mutate(Autogestao = ifelse(Autogestao == 1, rotulos[1],r
   xlab("Probabilidade de Autogestao acima da mediana") +
   theme_bw()
 
-# Avaliacao da regiao de suporte comum para Openness
-rotulos <- paste("HSE - Openness: ", c("Acima da media","Abaixo da mediana"))
-OpennessPredicted %>% mutate(Openness = ifelse(Openness == 1, rotulos[1],rotulos[2])) %>%
-  ggplot(aes(x = OpennessPScore)) +
+# Avaliacao da regiao de suporte comum para AberturaNovo
+rotulos <- paste("HSE - AberturaNovo: ", c("Muito Desenvolvido","A desenvolver"))
+AberturaNovoPredicted %>% mutate(AberturaNovo = ifelse(AberturaNovo == 1, rotulos[1],rotulos[2])) %>%
+  ggplot(aes(x = AberturaNovoPScore)) +
   geom_histogram(color = "white") +
-  facet_wrap(~Openness) +
-  xlab("Probabilidade de Openness acima da mediana") +
+  facet_wrap(~AberturaNovo) +
+  xlab("Probabilidade de AberturaNovo acima da mediana") +
   theme_bw()
 
 # Matching para Autogestao
 AutogestaoSemMissing <- dadosT %>% 
-  select(ProfComb, Autogestao, Openness, Autoconceito, ensinoMedioCompleto, one_of(vars_paream)) %>% 
+  select(ProfComb, Autogestao, AberturaNovo, Autoconceito, ensinoMedioCompleto, one_of(vars_paream)) %>% 
   na.omit()
 
 modMatchAutogestao <- matchit(Autogestao ~ id_real + idade1serie + sexoT + racaT + maeMedioCompleto,
@@ -113,15 +113,15 @@ modMatchAutogestao <- matchit(Autogestao ~ id_real + idade1serie + sexoT + racaT
 
 matchedAutogestao <- match.data(modMatchAutogestao)
 
-# Matching para Openness
-OpennessSemMissing <- dadosT %>% 
-  select(ProfComb, Autogestao, Openness, Autoconceito, ensinoMedioCompleto, one_of(vars_paream)) %>% 
+# Matching para AberturaNovo
+AberturaNovoSemMissing <- dadosT %>% 
+  select(ProfComb, Autogestao, AberturaNovo, Autoconceito, ensinoMedioCompleto, one_of(vars_paream)) %>% 
   na.omit()
 
-modMatchOpenness <- matchit(Openness ~ id_real + idade1serie+ sexoT + racaT + maeMedioCompleto,
-                            method = "nearest", discard = 'both', data = OpennessSemMissing)
+modMatchAberturaNovo <- matchit(AberturaNovo ~ id_real + idade1serie+ sexoT + racaT + maeMedioCompleto,
+                            method = "nearest", discard = 'both', data = AberturaNovoSemMissing)
 
-matchedOpenness <- match.data(modMatchOpenness)
+matchedAberturaNovo <- match.data(modMatchAberturaNovo)
 
 #Inspecao visual do matching
 fn_bal <- function(matchedAutogestao,variable) {
@@ -147,11 +147,11 @@ grid.arrange(
   nrow = 2, widths = c(1, 0.8)
 )
 
-fn_bal <- function(matchedOpenness,variable) {
-  matchedOpenness$variable <- matchedOpenness[,variable]
-  matchedOpenness$Openness <- as.factor(matchedOpenness$Openness)
-  support <- c(min(matchedOpenness$variable), max(matchedOpenness$variable))
-  ggplot(matchedOpenness, aes(x = distance, y = variable, color = Openness)) +
+fn_bal <- function(matchedAberturaNovo,variable) {
+  matchedAberturaNovo$variable <- matchedAberturaNovo[,variable]
+  matchedAberturaNovo$AberturaNovo <- as.factor(matchedAberturaNovo$AberturaNovo)
+  support <- c(min(matchedAberturaNovo$variable), max(matchedAberturaNovo$variable))
+  ggplot(matchedAberturaNovo, aes(x = distance, y = variable, color = AberturaNovo)) +
     geom_point(alpha = 0.2, size = 1.3) +
     geom_smooth(method = 'loess', se = F) +
     xlab('Propensity Score') +
@@ -161,10 +161,10 @@ fn_bal <- function(matchedOpenness,variable) {
 }
 
 grid.arrange(
-  fn_bal(matchedOpenness, 'id_real'),
-  fn_bal(matchedOpenness, 'sexoT'),
-  fn_bal(matchedOpenness, 'racaT'),
-  fn_bal(matchedOpenness, 'maeMedioCompleto'),
+  fn_bal(matchedAberturaNovo, 'id_real'),
+  fn_bal(matchedAberturaNovo, 'sexoT'),
+  fn_bal(matchedAberturaNovo, 'racaT'),
+  fn_bal(matchedAberturaNovo, 'maeMedioCompleto'),
   nrow = 2, widths = c(1, 0.8)
 )
 
@@ -174,7 +174,7 @@ mediaPosParAutogestao <- matchedAutogestao %>% group_by(Autogestao) %>%
   select(one_of(vars_paream)) %>% 
   summarise_all(funs(mean))
 
-mediaPosParOpenness <- matchedOpenness %>% group_by(Openness) %>% 
+mediaPosParAberturaNovo <- matchedAberturaNovo %>% group_by(AberturaNovo) %>% 
   select(one_of(vars_paream)) %>% 
   summarise_all(funs(mean))
 
@@ -182,8 +182,8 @@ testeParAutogestao <- lapply(vars_paream,function(v){
   t.test(matchedAutogestao[,v] ~ matchedAutogestao$Autogestao)
 })
 
-testeParOpenness <- lapply(vars_paream,function(v){
-  t.test(matchedOpenness[,v] ~ matchedOpenness$Openness)
+testeParAberturaNovo <- lapply(vars_paream,function(v){
+  t.test(matchedAberturaNovo[,v] ~ matchedAberturaNovo$AberturaNovo)
 })
 
 #Estimando os efeitos para HSE e salvando os resultados
@@ -192,6 +192,6 @@ didAutogestao <- lm(ProfComb ~ Autogestao + ensinoMedioCompleto + Autogestao*ens
                     data = matchedAutogestao)
 write.csv(tidy(didAutogestao), file = paste0(getwd(), '/Autogestao.csv'))
 
-didOpenness <- lm(ProfComb ~ Openness + ensinoMedioCompleto + Openness*ensinoMedioCompleto, 
-                  data = matchedOpenness)
-write.csv(tidy(didOpenness), file = paste0(getwd(), '/Openess.csv'))
+didAberturaNovo <- lm(ProfComb ~ AberturaNovo + ensinoMedioCompleto + AberturaNovo*ensinoMedioCompleto, 
+                  data = matchedAberturaNovo)
+write.csv(tidy(didAberturaNovo), file = paste0(getwd(), '/Openess.csv'))
